67b70ec921d9ee37af7dd561314ffa8fd28ab8b7
radmanlo <radman.lotfiazar@gmail.com>
2023-04-01 19:53:49 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here
+
+class Breakfast{
+
+    //constructor
+    constructor( food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+class Lunch{
+
+    // constructor
+    constructor( salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+class Dinner{
+    
+    //private variable
+    #dessert
+
+    //constructor
+    constructor( salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+    }
+
+}
